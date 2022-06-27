3c79b4e563d750bc6b2e54fb273b257e3e023299
srang03 <srang9503@gmail.com>
2022-06-27 18:26:46 +0900

    220627

---
+++ 1_HelloCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1_HelloCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+            
+        }
+    }
+}
