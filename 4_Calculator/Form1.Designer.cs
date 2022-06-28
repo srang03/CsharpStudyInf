18793c4352cf8c2efe85ce007c972c5ae6991b4b
unknown <srang9503@gmail.com>
2022-06-28 23:11:26 +0900

    220628

---
+++ 4_Calculator/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace _4_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_number1 = new System.Windows.Forms.Button();
+            this.btn_number2 = new System.Windows.Forms.Button();
+            this.btn_number3 = new System.Windows.Forms.Button();
+            this.btn_number4 = new System.Windows.Forms.Button();
+            this.btn_number5 = new System.Windows.Forms.Button();
+            this.btn_number6 = new System.Windows.Forms.Button();
+            this.btn_number7 = new System.Windows.Forms.Button();
+            this.btn_number8 = new System.Windows.Forms.Button();
+            this.btn_number9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.btn_plus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_number1
+            // 
+            this.btn_number1.Location = new System.Drawing.Point(12, 69);
+            this.btn_number1.Name = "btn_number1";
+            this.btn_number1.Size = new System.Drawing.Size(81, 74);
+            this.btn_number1.TabIndex = 0;
+            this.btn_number1.Text = "1";
+            this.btn_number1.UseVisualStyleBackColor = true;
+            this.btn_number1.Click += new System.EventHandler(this.btn_number1_Click);
+            // 
+            // btn_number2
+            // 
+            this.btn_number2.Location = new System.Drawing.Point(99, 69);
+            this.btn_number2.Name = "btn_number2";
+            this.btn_number2.Size = new System.Drawing.Size(81, 74);
+            this.btn_number2.TabIndex = 1;
+            this.btn_number2.Text = "2";
+            this.btn_number2.UseVisualStyleBackColor = true;
+            this.btn_number2.Click += new System.EventHandler(this.btn_number2_Click);
+            // 
+            // btn_number3
+            // 
+            this.btn_number3.Location = new System.Drawing.Point(186, 69);
+            this.btn_number3.Name = "btn_number3";
+            this.btn_number3.Size = new System.Drawing.Size(81, 74);
+            this.btn_number3.TabIndex = 2;
+            this.btn_number3.Text = "3";
+            this.btn_number3.UseVisualStyleBackColor = true;
+            // 
+            // btn_number4
+            // 
+            this.btn_number4.Location = new System.Drawing.Point(12, 149);
+            this.btn_number4.Name = "btn_number4";
+            this.btn_number4.Size = new System.Drawing.Size(81, 74);
+            this.btn_number4.TabIndex = 3;
+            this.btn_number4.Text = "4";
+            this.btn_number4.UseVisualStyleBackColor = true;
+            // 
+            // btn_number5
+            // 
+            this.btn_number5.Location = new System.Drawing.Point(99, 149);
+            this.btn_number5.Name = "btn_number5";
+            this.btn_number5.Size = new System.Drawing.Size(81, 74);
+            this.btn_number5.TabIndex = 4;
+            this.btn_number5.Text = "5";
+            this.btn_number5.UseVisualStyleBackColor = true;
+            // 
+            // btn_number6
+            // 
+            this.btn_number6.Location = new System.Drawing.Point(186, 149);
+            this.btn_number6.Name = "btn_number6";
+            this.btn_number6.Size = new System.Drawing.Size(81, 74);
+            this.btn_number6.TabIndex = 5;
+            this.btn_number6.Text = "6";
+            this.btn_number6.UseVisualStyleBackColor = true;
+            // 
+            // btn_number7
+            // 
+            this.btn_number7.Location = new System.Drawing.Point(12, 229);
+            this.btn_number7.Name = "btn_number7";
+            this.btn_number7.Size = new System.Drawing.Size(81, 74);
+            this.btn_number7.TabIndex = 6;
+            this.btn_number7.Text = "7";
+            this.btn_number7.UseVisualStyleBackColor = true;
+            // 
+            // btn_number8
+            // 
+            this.btn_number8.Location = new System.Drawing.Point(99, 229);
+            this.btn_number8.Name = "btn_number8";
+            this.btn_number8.Size = new System.Drawing.Size(81, 74);
+            this.btn_number8.TabIndex = 6;
+            this.btn_number8.Text = "8";
+            this.btn_number8.UseVisualStyleBackColor = true;
+            // 
+            // btn_number9
+            // 
+            this.btn_number9.Location = new System.Drawing.Point(186, 229);
+            this.btn_number9.Name = "btn_number9";
+            this.btn_number9.Size = new System.Drawing.Size(81, 74);
+            this.btn_number9.TabIndex = 6;
+            this.btn_number9.Text = "9";
+            this.btn_number9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(186, 309);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(81, 74);
+            this.button10.TabIndex = 6;
+            this.button10.Text = "button7";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(99, 309);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(81, 74);
+            this.button11.TabIndex = 6;
+            this.button11.Text = "button7";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(12, 309);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(81, 74);
+            this.button12.TabIndex = 6;
+            this.button12.Text = "button7";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.BackColor = System.Drawing.Color.White;
+            this.lbl_result.Font = new System.Drawing.Font("굴림", 12F);
+            this.lbl_result.Location = new System.Drawing.Point(12, 9);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(408, 49);
+            this.lbl_result.TabIndex = 7;
+            this.lbl_result.Text = "0";
+            this.lbl_result.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btn_plus
+            // 
+            this.btn_plus.Location = new System.Drawing.Point(338, 69);
+            this.btn_plus.Name = "btn_plus";
+            this.btn_plus.Size = new System.Drawing.Size(81, 74);
+            this.btn_plus.TabIndex = 8;
+            this.btn_plus.Text = "+";
+            this.btn_plus.UseVisualStyleBackColor = true;
+            this.btn_plus.Click += new System.EventHandler(this.btn_plus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 407);
+            this.Controls.Add(this.btn_plus);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.btn_number9);
+            this.Controls.Add(this.btn_number8);
+            this.Controls.Add(this.btn_number7);
+            this.Controls.Add(this.btn_number6);
+            this.Controls.Add(this.btn_number5);
+            this.Controls.Add(this.btn_number4);
+            this.Controls.Add(this.btn_number3);
+            this.Controls.Add(this.btn_number2);
+            this.Controls.Add(this.btn_number1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_number1;
+        private System.Windows.Forms.Button btn_number2;
+        private System.Windows.Forms.Button btn_number3;
+        private System.Windows.Forms.Button btn_number4;
+        private System.Windows.Forms.Button btn_number5;
+        private System.Windows.Forms.Button btn_number6;
+        private System.Windows.Forms.Button btn_number7;
+        private System.Windows.Forms.Button btn_number8;
+        private System.Windows.Forms.Button btn_number9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.Button btn_plus;
+    }
+}
+
---
+++ 4_Calculator/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_Calculator
+{
+    public partial class Form1 : Form
+    {
+        private string pre_number;
+        private string next_number;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            pre_number = string.Empty;
+            next_number = string.Empty;
+        }
+
+        private void btn_number1_Click(object sender, EventArgs e)
+        {
+            SetNum("1");
+
+
+        }
+
+        private void btn_number2_Click(object sender, EventArgs e)
+        {
+            SetNum("2");
+        }
+
+        public void SetNum(string number)
+        {
+            if (this.lbl_result.Text == "0")
+            {
+                this.lbl_result.Text = number;
+            }
+            else
+            {
+                this.lbl_result.Text += number;
+            }
+        }
+
+        private void btn_plus_Click(object sender, EventArgs e)
+        {
+         // 숫자 입력
+         // 더하기 버튼 클릭 => num1에 저장
+         // 숫자 입력
+         // 더하기 버튼 클릭 => num
+        }
+    }
+}
