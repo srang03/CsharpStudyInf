864ec8c1af652cb23bf75925188e7546688226e0
srang03 <srang9503@gmail.com>
2022-07-27 17:56:03 +0900

    220727 XMLReader

---
+++ Dori_17XMLReader/Form1.cs
@@ -0,0 +1,54 @@
+﻿using Dori17_StreamReaderReivew;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dori_17XMLReader
+{
+    
+    public partial class Form1 : Form
+    {
+        XMLClass _xml = new XMLClass();
+
+        Dictionary<string, string> _dData = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_setConfig_Click(object sender, EventArgs e)
+        {
+            var file_path = Application.StartupPath + "\\config.xml";
+            _dData.Clear();
+            _dData.Add(XMLClass._TEXT_DATA, this.txb_name.Text);
+            _dData.Add(XMLClass._NUMBERBOX_DATA, this.nup_number.Value.ToString());
+            _dData.Add(XMLClass._CBOX_DATA, this.checkbx_set.Checked.ToString());
+
+            _xml.XMLWriter(file_path, _dData);
+
+        }
+
+        private void btn_getConfig_Click(object sender, EventArgs e)
+        {
+            var file_path = Application.StartupPath + "\\config.xml";
+            _dData.Clear();
+            _dData = _xml.XMLReader(file_path);
+
+            this.txb_name.Text = _dData[XMLClass._TEXT_DATA];
+            this.nup_number.Value = Convert.ToInt32(_dData[XMLClass._NUMBERBOX_DATA]);
+            this.checkbx_set.Checked = bool.Parse(_dData[XMLClass._CBOX_DATA]);
+        }
+    }
+}
---
+++ Dori_17XMLReader/XMLClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Dori17_StreamReaderReivew
+{
+    public class XMLClass
+    {
+        public static string _TEXT_DATA = "TEXT_DATA";
+        public static string _CBOX_DATA = "CBOX_DATA";
+        public static string _NUMBERBOX_DATA = "NUMBERBOX_DATA";
+
+        public Dictionary<string,string> XMLReader(string strXMLPath)
+        {
+            Dictionary<string, string> DXMLConfig = new Dictionary<string, string>();
+
+            using (XmlReader xrd= XmlReader.Create(strXMLPath))
+            {
+                while (xrd.Read())
+                {
+                    if (xrd.IsStartElement())
+                    {
+                        if (xrd.Name.Equals("SETTING"))
+                        {
+                            //string strID = xrd["ID"];
+                            xrd.Read();
+
+                            string strText = xrd.ReadElementContentAsString(_TEXT_DATA, "");
+                            DXMLConfig.Add(_TEXT_DATA, strText);
+
+                            string strCBOX = xrd.ReadElementContentAsString(_CBOX_DATA, "");
+                            DXMLConfig.Add(_CBOX_DATA, strCBOX);
+
+                            string strNumberBox = xrd.ReadElementContentAsString(_NUMBERBOX_DATA, "");
+                            DXMLConfig.Add(_NUMBERBOX_DATA, strNumberBox);
+                        }
+                    }
+                    
+                }
+            }
+                return DXMLConfig;
+        }
+
+        public void XMLWriter(string strXMLPath, Dictionary<string, string> DXMLConfig)
+        {
+            using(XmlWriter xwr = XmlWriter.Create(strXMLPath))
+            {
+                xwr.WriteStartDocument();
+
+                // SETTING
+                xwr.WriteStartElement("SETTING");
+                xwr.WriteAttributeString("ID", "0001");
+
+                xwr.WriteElementString(_TEXT_DATA, DXMLConfig[_TEXT_DATA]);
+                xwr.WriteElementString(_CBOX_DATA, DXMLConfig[_CBOX_DATA]);
+                xwr.WriteElementString(_NUMBERBOX_DATA, DXMLConfig[_NUMBERBOX_DATA]);
+
+                xwr.WriteEndElement();
+                xwr.WriteEndDocument();
+            }
+        }
+
+
+    }
+}
