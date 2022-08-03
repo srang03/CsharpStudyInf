39e7c2961d9f7cefa81fea106f62a568ff147471
srang03 <srang9503@gmail.com>
2022-08-03 18:23:14 +0900

    220803

---
+++ 03_EventReview/Designer.cs
@@ -0,0 +1,29 @@
+﻿namespace _03_EventReview
+{
+    internal class Designer
+    {
+        public delegate void DelFull();
+        public event DelFull OnFull;
+        int iCount;
+        public Designer()
+        {
+            iCount = 0;
+        }
+
+        internal void DesignPlease()
+        {
+            iCount += 1;
+            if (iCount % 5 == 0)
+            {
+                if(OnFull != null)
+                {
+                    OnFull();
+                }
+                else
+                {
+                    System.Console.WriteLine("이벤트를 등록해주세요");
+                }
+            }
+        }
+    }
+}
