ffb417e285b325c4a8d1efe2571fb5f15cabdfdc
unknown <srang9503@gmail.com>
2022-08-10 14:42:49 +0900

    220810 exception

--- Dori28_Exception/Form1.Designer.cs
+++ Dori28_Exception/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "실행";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 39);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(121, 145);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Dori28_Exception/Form1.cs
+++ Dori28_Exception/Form1.cs
@@ -16,5 +16,66 @@ namespace Dori28_Exception
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // FormatException 발생
+                if(int.TryParse(this.textBox1.Text, out int iNum))
+                {
+                    this.label1.Text = iNum.ToString();
+                }
+                else
+                {
+                    this.label1.Text = "DataFrom이 맞지 않습니다";
+                }
+
+                // ArgumentOutOfRangeException 발생
+                List<string> ListStr = new List<string>();
+                ListStr.Add("Test");
+                
+                
+                for(int i = 0; i<2; i++)
+                {
+                    var strTest = ListStr[i];
+                }
+
+                // NullReference Exception 발생
+                List<object> ListObj = null;
+                var a = ListObj[0];
+
+                // InvalidCastException 발생
+                object oTest = "ff123";
+                int iTest2 = (int)oTest;
+
+                // 예외처리 방지 방법
+                if (oTest is int)
+                {
+                    int iTest3 = (int)oTest;
+                }
+            }
+            catch(FormatException ex)
+            {
+                this.label1.Text = ex.ToString();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                this.label1.Text = ex.ToString();
+            }
+            catch (NullReferenceException ex)
+            {
+                this.label1.Text = ex.ToString();
+            }
+            catch (InvalidCastException ex)
+            {
+                this.label1.Text = ex.ToString();
+            }
+
+            finally
+            {
+
+            }
+        }
     }
 }
