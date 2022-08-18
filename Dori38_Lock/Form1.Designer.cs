d624f492250341c0f859cff4840c3f496544ddd8
unknown <srang9503@gmail.com>
2022-08-18 22:21:05 +0900

    220818 thread

---
+++ Dori38_Lock/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Dori38_Lock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_room1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.group2 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btn_room2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.btn_room1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 159);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Room1";
+            // 
+            // btn_room1
+            // 
+            this.btn_room1.Location = new System.Drawing.Point(6, 41);
+            this.btn_room1.Name = "btn_room1";
+            this.btn_room1.Size = new System.Drawing.Size(79, 100);
+            this.btn_room1.TabIndex = 0;
+            this.btn_room1.Text = "button1";
+            this.btn_room1.UseVisualStyleBackColor = true;
+            this.btn_room1.Click += new System.EventHandler(this.btn_room1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(91, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(256, 100);
+            this.listBox1.TabIndex = 1;
+            // 
+            // group2
+            // 
+            this.group2.Controls.Add(this.listBox2);
+            this.group2.Controls.Add(this.btn_room2);
+            this.group2.Location = new System.Drawing.Point(393, 12);
+            this.group2.Name = "group2";
+            this.group2.Size = new System.Drawing.Size(353, 159);
+            this.group2.TabIndex = 0;
+            this.group2.TabStop = false;
+            this.group2.Text = "Room2";
+            this.group2.UseCompatibleTextRendering = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(91, 41);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(256, 100);
+            this.listBox2.TabIndex = 1;
+            // 
+            // btn_room2
+            // 
+            this.btn_room2.Location = new System.Drawing.Point(6, 41);
+            this.btn_room2.Name = "btn_room2";
+            this.btn_room2.Size = new System.Drawing.Size(79, 100);
+            this.btn_room2.TabIndex = 0;
+            this.btn_room2.Text = "button1";
+            this.btn_room2.UseVisualStyleBackColor = true;
+            this.btn_room2.Click += new System.EventHandler(this.btn_room2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "사용 현황";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 12;
+            this.listBox3.Location = new System.Drawing.Point(12, 320);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(736, 124);
+            this.listBox3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 218);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(734, 86);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.group2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.group2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_room1;
+        private System.Windows.Forms.GroupBox group2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btn_room2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Dori38_Lock/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dori38_Lock
+{
+    public partial class Form1 : Form
+    {
+        Thread T1;
+        Thread T2;
+        object RoomLock = new object();
+        object RoomLock2 = new object();
+
+        int iRoom1Count = 1;
+        int iRoom2Count = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_room1_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Add($"Room 1 : {iRoom1Count}번 예약");
+            T1 = new Thread(new ParameterizedThreadStart(Run));
+            T1.Start(iRoom1Count);
+            iRoom1Count += 1;
+        }
+
+        private void btn_room2_Click(object sender, EventArgs e)
+        {
+            this.listBox2.Items.Add($"Room 2 : {iRoom2Count}번 예약");
+            T1 = new Thread(new ParameterizedThreadStart(Run2));
+            T1.Start(iRoom2Count);
+            iRoom2Count += 1;
+        }
+
+        private void Run(object obj)
+        {
+           
+            lock (RoomLock)
+            {
+                InvokeFunction(this.textBox1, $"Room 1 : {obj}번 사용 중");
+                for (int i = 0; i<5; i++)
+                {
+                    if (this.InvokeRequired)
+                    {
+                         Invoke(new Action(delegate {
+                             listBox3.Items.Add($"Room 1 : {iRoom1Count}번 ... {i} sec ");
+
+                    }));
+                        Console.WriteLine(i);
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+            InvokeFunction(this.textBox1, string.Empty);
+            Thread.Sleep(1000);
+        }
+
+        private void InvokeFunction(TextBox textbox, string str)
+        {
+            if (this.InvokeRequired)
+            {
+                Invoke(new Action(delegate
+                {
+                    textbox.Text = str;
+                }));
+            }
+        }
+
+        private void Run2(object obj)
+        {
+
+            lock (RoomLock)
+            {
+                InvokeFunction(this.textBox1, $"Room 2 : {obj}번 사용 중");
+                for (int i = 0; i < 5; i++)
+                {
+                    if (this.InvokeRequired)
+                    {
+                        Invoke(new Action(delegate {
+                            listBox3.Items.Add($"Room 2 : {iRoom2Count}번 ... {i} sec ");
+
+                        }));
+                        Console.WriteLine(i);
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+            InvokeFunction(this.textBox1, string.Empty);
+            Thread.Sleep(1000);
+        }
+
+    }
+
+}
