9bc090f659eacfd650ba1aedb8a5b4b8df8a7ba0
unknown <srang9503@gmail.com>
2022-08-28 23:03:03 +0900

    220828

---
+++ 1_HelloCSharp/CSharpThread/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CSharpThread
+{
+    class Restaurant
+    {
+        public void MakeFood()
+        {
+            Console.WriteLine("요리시작");
+            DateTime startTime = DateTime.Now;
+            void Egg()
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("계란 요리");
+            }
+            Thread t1 = new Thread(new ThreadStart(Egg));
+            
+            void Soup()
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("국 요리");
+            }
+            Thread t2 = new Thread(new ThreadStart(Soup));
+            
+            void Steak()
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("스테이크 요리");
+            }
+            Thread t3 = new Thread(new ThreadStart(Steak));
+            t1.Start(); t2.Start(); t3.Start(); // async
+
+            t1.Join(); t2.Join(); t3.Join(); // await
+
+            Console.WriteLine("요리종료");
+            DateTime EndTime = DateTime.Now;
+            Console.WriteLine((EndTime - startTime).TotalSeconds);
+
+            Console.WriteLine("식사 시작");
+            //SinglePro();
+            MultiPro();
+            Console.WriteLine("식사 종료");
+
+        }
+
+        private void SinglePro()
+        {
+            for (int i = 0; i < 20_000; i++)
+            {
+                Console.WriteLine("수다...");
+            }
+        }
+        private void MultiPro()
+        {
+            Parallel.For(0, 20_000, (i) => { global::System.Console.WriteLine($"수다 {i}"); });
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Restaurant().MakeFood();
+        }
+    }
+}
---
+++ CSharpThread/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpThread
+{
+    public class Restaurant
+    {
+
+        public string MakeFood()
+        {
+            return "밥을 짓다";
+        }
+    }
+    public partial class Form1 : Form
+    {
+        ListBox lbx;
+        public Form1()
+        {
+            InitializeComponent();
+            lbx = new ListBox();
+            this.Controls.Add(lbx);
+
+            Restaurant restaurant = new Restaurant();
+            lbx.Items.Add(restaurant.MakeFood());
+        }
+    }
+}
