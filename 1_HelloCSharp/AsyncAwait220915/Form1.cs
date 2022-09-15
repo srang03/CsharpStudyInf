aba6a31e71844e452fa5a1874fa2c519b0f1a3dc
srang03 <srang9503@gmail.com>
2022-09-15 18:37:58 +0900

    220915

---
+++ 1_HelloCSharp/AsyncAwait220915/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait220915
+{
+    public partial class Form1 : Form
+    {
+        Task t1;
+        Task t2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t1 = ReadDeviceAsync();
+            t2 = ReadInternalStorageAync();
+        }
+
+        private async Task ReadInternalStorageAync()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task ReadDeviceAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
