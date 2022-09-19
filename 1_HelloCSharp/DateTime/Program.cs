7494ce9231badacf7ad27143fd76333a1ca58d7f
srang03 <srang9503@gmail.com>
2022-09-19 18:23:06 +0900

    220919

---
+++ 1_HelloCSharp/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string deviceId = "D324DFS";
+            string tmp = "{0: yyyy년MM월dd일}" + deviceId;
+            Console.WriteLine(string.Format(tmp, DateTime.Now));
+            
+            double temp = ((double)7 / 96 * 100);
+            Console.WriteLine(temp);
+            Console.ReadLine();
+        }
+    }
+}
